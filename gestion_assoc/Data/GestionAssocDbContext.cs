52b02ca5bc21392a43b2570abbdb081d0986b4c8
Ousseini Adamou <adnanejiko@gmail.com>
2022-08-21 22:20:40 +0000

    crud association

---
+++ gestion_assoc/Data/GestionAssocDbContext.cs
@@ -0,0 +1,14 @@
+﻿using gestion_assoc.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace gestion_assoc.Data
+{
+    public class GestionAssocDbContext : DbContext
+    {
+        public GestionAssocDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Association> Associations { get; set; }
+    }
+}
---
+++ gestion_assoc/Migrations/20220820011059_Association.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gestion_assoc.Migrations
+{
+    public partial class Association : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Associations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Nom = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Annee_Creation = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Tel_1 = table.Column<int>(type: "int", nullable: false),
+                    Tel_2 = table.Column<int>(type: "int", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Adresse = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Associations", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Associations");
+        }
+    }
+}
